6058bb9ed72ffa1d899806130ac16c0643bb3692
Mohammed Na Habar <mohammed.na.habbar@gmail.com>
2023-03-30 16:09:40 +0300

    Init And Design Main Activity

--- app/src/main/java/com/biosteam99/unittestingexample1/MainActivity.kt
+++ app/src/main/java/com/biosteam99/unittestingexample1/MainActivity.kt
@@ -2,10 +2,16 @@ package com.biosteam99.unittestingexample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.biosteam99.unittestingexample1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding :ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
     }
 }

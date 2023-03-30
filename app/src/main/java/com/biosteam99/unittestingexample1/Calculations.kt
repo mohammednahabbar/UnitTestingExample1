9078af8507e9f2430987041d8dd1dd240046b048
Mohammed Na Habar <mohammed.na.habbar@gmail.com>
2023-03-30 15:50:34 +0300

    Create Calculations.kt interface

---
+++ app/src/main/java/com/biosteam99/unittestingexample1/Calculations.kt
@@ -0,0 +1,8 @@
+package com.biosteam99.unittestingexample1
+
+interface Calculations {
+
+    fun areaCircleCalculate(radius:Double):Double
+    fun circumferenceCircleCalculate(radius: Double):Double
+
+}
